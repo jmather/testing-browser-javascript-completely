cf080fee7f1eff90201d2518f7e45131ba11e5be
Jacob Mather <jmather@mashery.com>
2014-10-21 11:01:55 -0700

    Fixing code issues from issue #1, and added more detail to the package.json

--- scripts/support/run-coverage-test.sh
+++ scripts/support/run-coverage-test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"/../.. 1>/dev/null 2>&1)" && pwd )"
 
-ISTANBUL=$DIR/node_modules/istanbul/lib/cli.js
-GRUNT=$DIR/node_modules/.bin/grunt
+ISTANBUL="$DIR/node_modules/istanbul/lib/cli.js"
+GRUNT="$DIR/node_modules/.bin/grunt"
 
 CI_MODE=0
 
@@ -12,43 +12,44 @@ then
     CI_MODE=1
 fi
 
-if [ -d build/reports/code-coverage ]
+if [ -d "$DIR/build/reports/code-coverage" ]
 then
-    rm -rf build/reports/code-coverage
+    rm -rf "$DIR/build/reports/code-coverage"
 fi
 
-if [ -d coverage ]
+echo $DIR/coverage
+
+if [ -d "$DIR/coverage" ]
 then
     rm -rf "$DIR/coverage"
 fi
 
-if [ ! -d build/logs ]
+if [ ! -d "$DIR/build/logs" ]
 then
-    mkdir -p build/logs
+    mkdir -p "$DIR/build/logs"
 fi
 
-if [ ! -d build/reports/code-coverage ]
+if [ ! -d "$DIR/build/reports/code-coverage" ]
 then
-    mkdir -p build/reports/code-coverage
+    mkdir -p "$DIR/build/reports/code-coverage"
 fi
 
 mkdir -p "$DIR/coverage/app"
 mkdir -p "$DIR/coverage/out"
 
-$ISTANBUL instrument --no-compact --complete-copy -o "$DIR/coverage/app/scripts" app/scripts
+"$ISTANBUL" instrument --no-compact --complete-copy -o "$DIR/coverage/app/scripts" "$DIR/app/scripts"
 
 ln -sf "$DIR/app/vendor" "$DIR/coverage/app/vendor"
 
-$GRUNT test:coverage:process
+"$GRUNT" test:coverage:process
 
 if [ "$CI_MODE" == "0" ]
 then
-    $ISTANBUL report html --root coverage/out --dir build/reports/code-coverage/
-    open build/reports/code-coverage/index.html
+    "$ISTANBUL" report html --root "$DIR/coverage/out" --dir "$DIR/build/reports/code-coverage/"
+    open "$DIR/build/reports/code-coverage/index.html"
 else
-    $ISTANBUL report cobertura --root coverage/out/ --dir coverage/report -v
-    mv coverage/report/cobertura-coverage.xml build/logs/cobertura-coverage.xml
+    "$ISTANBUL" report cobertura --root "$DIR/coverage/out/" --dir "$DIR/coverage/report" -v
+    mv "$DIR/coverage/report/cobertura-coverage.xml" "$DIR/build/logs/cobertura-coverage.xml"
 fi
 
-rm -rf "$DIR/coverage"
-
+#rm -rf "$DIR/coverage"
